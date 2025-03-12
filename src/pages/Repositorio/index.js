576a029d7b7df4b111b708c8d6e6d32b22df28de
Joanadayse <daysejoana3@gmail.com>
2025-03-12 20:47:50 -0300

    carregando detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,12 +1,52 @@
 import { useParams } from "react-router-dom";
+import { Container } from "./styled";
+import { useEffect, useState } from "react";
+import api from "../../services/api"
 
 export default function Repositorio() {
+  const { repositorio } = useParams();
+  const [repo , setRepo]= useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading,setLoading]=useState(true);
 
-   const { repositorio } = useParams();
+  useEffect(() => {
+    async function loadData(params) {
+      try {
+        const nomeRepo = decodeURIComponent(repositorio);
+
+        const [repoResponse, issuesResponse] = await Promise.all([
+          api.get(`/repos/${nomeRepo}`),
+          api.get(`/repos/${nomeRepo}/issues`, {
+            params: {
+              state: "open",
+              per_page: 5,
+            },
+          }),
+        ]);
+  setRepo(repoResponse.data);
+  setIssues(issuesResponse.data);
+  setLoading(false)
+        console.log("Repositório:", repoResponse.data);
+        console.log("Issues:", issuesResponse.data);
+      } catch (error) {
+        console.error(
+          "Erro na requisição:",
+          error.response?.data || error.message
+        );
+      }
+    }
+
+    if (repositorio) {
+      loadData();
+    }
+  }, [repositorio]);
+
+   
   return (
     <>
-      <h1 style={{ color: "#fff" }}>
-        {decodeURIComponent( repositorio)}</h1>;
+      <Container >
+        {/* {decodeURIComponent( repositorio)} */}
+      </Container>;
     </>
   );
 }
